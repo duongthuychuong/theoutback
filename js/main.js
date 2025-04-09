f0a5f112fd4acbe13ee2e94288e4c065fdccfd0d
ngocthai1232 <ngocthai25042005@gmail.com>
2025-04-09 11:31:56 +0700

    menu bar

--- js/main.js
+++ js/main.js
@@ -9,3 +9,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("nav").classList.toggle("change");
+}
